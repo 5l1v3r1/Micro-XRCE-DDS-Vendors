241d969b51d97b3423673301146c6136ca3d57ee
jfm92 <jfloresm92@gmail.com>
2019-11-15 12:45:31 +0100

    Improvement on the toolchain for the crazyflie

--- Crazyflie/toolchain/cf_toolchain.cmake
+++ Crazyflie/toolchain/cf_toolchain.cmake
@@ -10,13 +10,10 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(PLATFORM_NAME "CrazyFlie")
 set(CMAKE_TRANSPORT_DEFINE "SERIAL" CACHE STRING "Transport for this platform")
 
-set(CROSSDEV "" CACHE STRING "GCC compiler use in freeRTOS.")
-set(ARCH_CPU_FLAGS "" CACHE STRING "Makefile arquitecture flags.")
-set(ARCH_OPT_FLAGS "" CACHE STRING "Makefile optimization flags.")
+set(CMAKE_SYSROOT CMAKE_SYSROOT_PATH)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
-
-set(CMAKE_EXTERNAL_FILES ${CMAKE_CURRENT_SOURCE_DIR} CACHE PATH "")
 
 set(CMAKE_C_FLAGS_INIT "-std=c11 -DARM_MATH_CM4 -D__FPU_PRESENT=1 -D__TARGET_FPU_VFP  -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mcpu=cortex-m4 -mthumb -ffunction-sections -fdata-sections" CACHE STRING "" FORCE)
 
